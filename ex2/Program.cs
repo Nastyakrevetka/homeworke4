f0fc1825ba983c3aebccba728b32ee5e8b59ced7
Nastya_krevtka <nastyamkaka1278@gmail.com>
2022-12-22 09:46:22 +0300

    задача2  сумма цифр в числе

---
+++ ex2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+
+Console.WriteLine("введите число: ");
+int numb = int.Parse(Console.ReadLine());
+int sum = 0;
+while (numb > 0)
+{
+
+    sum = sum + numb % 10;
+    numb = numb / 10;
+
+}
+Console.WriteLine($"сумма цифр числа {numb} равна: {sum}");
