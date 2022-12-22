56e4c8d31bf06f53078e764a427c2a8b1e8b62b6
Nastya_krevtka <nastyamkaka1278@gmail.com>
2022-12-22 10:46:27 +0300

    задача3 вывод массива

---
+++ ex3/Program.cs
@@ -0,0 +1,34 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+int DataInput (string str)
+{
+    Console.Write(str);
+    int number= int.Parse(Console.ReadLine());
+    return number;
+}
+
+int [] FillArray(int[] array)
+{
+    int length= array.Length;
+    int index=0;
+    for( int i=0; i<length;i++)
+    {
+        array[i]= new Random().Next(0,1000);
+    }
+    return array;
+}
+void PrintArray (int[] col)
+{
+    int count= col.Length;
+    int i;
+    for (i=0; i<count; i++)
+    {
+        Console.WriteLine( col[i]+ "");
+    }
+}
+int Number=DataInput("Введите число: ");
+int[] array= new int[Number];
+array= FillArray(array);
+PrintArray(array);
